c6ddfb1a2e615c8dbf0109e1270bd0390b9be23c
qvarkk <xumdes@gmail.com>
2025-02-11 20:09:45 +0500

    bootstrapped project and added other dependencies

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('App\Http\Controllers\Main')->group(function () {
+    Route::get('/', IndexController::class)->name('main.index');
+});
+
+Route::namespace('App\Http\Controllers\Post')->prefix('posts')->group(function () {
+    Route::get('/', IndexController::class)->name('post.index');
+});
+
+Route::namespace('App\Http\Controllers\Admin')->prefix('admin')->group(function () {
+    Route::namespace('Main')->group(function () {
+       Route::get('/', IndexController::class)->name('admin.main.index');
+    });
+
+    Route::namespace('Post')->prefix('posts')->group(function () {
+        Route::get('/', IndexController::class)->name('admin.post.index');
+        Route::post('/', StoreController::class)->name('admin.post.store');
+    });
+
+    Route::namespace('Category')->prefix('categories')->group(function () {
+        Route::get('/', IndexController::class)->name('admin.category.index');
+        Route::get('/create', CreateController::class)->name('admin.category.create');
+        Route::post('/', StoreController::class)->name('admin.category.store');
+        Route::get('/{category}', ShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', EditController::class)->name('admin.category.edit');
+        Route::patch('/{category}', UpdateController::class)->name('admin.category.update');
+        Route::delete('/{category}', DestroyController::class)->name('admin.category.destroy');
+    });
 });
