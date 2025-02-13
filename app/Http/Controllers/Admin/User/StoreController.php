926a65e98745f1d1748bb1d7b63cfb0d10a68628
qvarkk <xumdes@gmail.com>
2025-02-13 12:13:34 +0500

    copied admin/user controllers from old project and updated user update request

---
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\User\StoreRequest;
+use App\Jobs\StoreUserJob;
+use App\Mail\User\PasswordMail;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+class StoreController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(StoreRequest $request)
+    {
+        $data = $request->validated();
+
+        // TODO: send random password in email and make it queueable
+        // StoreUserJob::dispatch($data);
+
+        $password = Str::random(16);
+        $data['password'] = Hash::make($password);
+
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
+
+        return redirect()->route('admin.user.index');
+    }
+}
--- app/Http/Requests/Admin/User/UpdateRequest.php
+++ app/Http/Requests/Admin/User/UpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Admin\User;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateRequest extends FormRequest
 {
@@ -23,7 +24,14 @@ class UpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $this->user_id],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                'unique:users,email,' . $this->user_id,
+                // Rule::unique('users')->ignore($this->user_id),
+            ],
             'user_id' => ['required', 'integer', 'exists:users,id'],
             'role' => ['integer'],
         ];
